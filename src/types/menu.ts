08949a5e37336de8829051683ae084bd3fb061b2
Xetha666 <bryans27092000@gmail.com>
2026-04-14 12:24:14 -0500

    feat: add Menu interface and options for navigation

---
+++ src/types/menu.ts
@@ -0,0 +1,24 @@
+interface Menu {
+  name: string;
+  href: string;
+}
+
+export const Options: Menu[] = 
+[
+  {
+    name: "Inicio",
+    href: "#hero",
+  },
+  {
+    name: "Proyectos",
+    href: "#projects",
+  },
+  {
+    name: "Sobre mi",
+    href: "#about",
+  },
+  {
+    name: "Contacto",
+    href: "#contact",
+  },
+]
