eaee207f6e4429a2354a6df3c62174026c4a0844
Xetha666 <bryans27092000@gmail.com>
2026-04-19 00:35:37 -0500

    feat: enable advanced mode in Cloudflare adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://bryansantillan.dev',
   adapter: cloudflare({
+    mode: 'advanced',
     platformProxy: {
       enabled: true
     }
