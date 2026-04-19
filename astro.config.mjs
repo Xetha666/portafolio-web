dea8c5c75d9dc8f113711e2f2e4c4d97f3061c3f
Xetha666 <bryans27092000@gmail.com>
2026-04-19 00:32:02 -0500

    feat: enable platformProxy in cloudflare adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://bryansantillan.dev',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   output: 'server',
 
   session: {
